296f91245bae3b92fb9da1a8127b5981d97f3726
Christopher Jobst and George Dean <pair+cjobst+gdean@pivotallabs.com>
2014-03-10 11:24:24 -0700

    Create folder structure and add Cedar Framework.

---
+++ ios/BiteSized/Controllers/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BiteSized
+//
+//  Created by pivotal on 3/10/14.
+//  Copyright (c) 2014 pivotal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ios/Specs/Supporting Files/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Specs
+//
+//  Created by pivotal on 3/10/14.
+//  Copyright (c) 2014 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
